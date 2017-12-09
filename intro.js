b79353f8348144b98449292f7898fb62d91f5bfd
jenyu7 <jyu7@stuy.edu>
2017-12-09 18:03:49 -0500

    mouseover working

--- intro.js
+++ intro.js
@@ -8,22 +8,36 @@ var fibonacci = function(n){
     else{return fibonacci(n-1) + fibonacci(n-2);}
 };
 
-var num = 0;
+var num = 1;
 
 var fibButton = function(){
     console.log(fibonacci(num));
-    num += 1;
     var el = document.getElementById("thelist");
     item = document.createElement("li");
     item.innerHTML=fibonacci(num);
     el.appendChild(item);
-    //el.appendChild(num);
-    //el.createElement("li").appendChild(num);
+    num += 1;
+    updateList();
 };
 
 var newEl = document.getElementById("newEl");
 newEl.addEventListener('click', fibButton);
 
+var list;
+var updateList = function(){
+    list = document.getElementsByTagName("li");
+    console.log(list);
+    for (var i = 0; i < list.length; i++){
+	list[i].addEventListener('mouseover', changeHead);
+	console.log(list[i]);
+    }
+}
+
+var changeHead = function(n){
+    //console.log(n);
+    var head = document.getElementById("h");
+    head.innerHTML = this.innerHTML;
+};
 /*
 Write a JavaScript file that will do the following:
 Upon button push, add an element to the list.
